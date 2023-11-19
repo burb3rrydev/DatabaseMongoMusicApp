cde993565683fa8c15223fc13c19ec4d438d7deb
Burb3rry Dev <burb3rrydev@gmail.com>
2023-11-19 14:38:10 -0700

    Add project files.

---
+++ DatabaseSQLMusicApp/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace DatabaseSQLMusicApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tbDescription = new System.Windows.Forms.TextBox();
+            this.tbImageURL = new System.Windows.Forms.TextBox();
+            this.tbYear = new System.Windows.Forms.TextBox();
+            this.tbArtist = new System.Windows.Forms.TextBox();
+            this.tbAlbumName = new System.Windows.Forms.TextBox();
+            this.lbDescription = new System.Windows.Forms.Label();
+            this.lbImageURL = new System.Windows.Forms.Label();
+            this.lbYear = new System.Windows.Forms.Label();
+            this.lbArtist = new System.Windows.Forms.Label();
+            this.lbAlbumName = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.lblTracks = new System.Windows.Forms.Label();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(239, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load Albums";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(239, 44);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(639, 190);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(638, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(373, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(884, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(194, 190);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.tbDescription);
+            this.groupBox1.Controls.Add(this.tbImageURL);
+            this.groupBox1.Controls.Add(this.tbYear);
+            this.groupBox1.Controls.Add(this.tbArtist);
+            this.groupBox1.Controls.Add(this.tbAlbumName);
+            this.groupBox1.Controls.Add(this.lbDescription);
+            this.groupBox1.Controls.Add(this.lbImageURL);
+            this.groupBox1.Controls.Add(this.lbYear);
+            this.groupBox1.Controls.Add(this.lbArtist);
+            this.groupBox1.Controls.Add(this.lbAlbumName);
+            this.groupBox1.Location = new System.Drawing.Point(12, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(221, 190);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add Album";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(19, 161);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Add";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tbDescription
+            // 
+            this.tbDescription.Location = new System.Drawing.Point(104, 137);
+            this.tbDescription.Name = "tbDescription";
+            this.tbDescription.Size = new System.Drawing.Size(100, 20);
+            this.tbDescription.TabIndex = 15;
+            // 
+            // tbImageURL
+            // 
+            this.tbImageURL.Location = new System.Drawing.Point(104, 106);
+            this.tbImageURL.Name = "tbImageURL";
+            this.tbImageURL.Size = new System.Drawing.Size(100, 20);
+            this.tbImageURL.TabIndex = 14;
+            // 
+            // tbYear
+            // 
+            this.tbYear.Location = new System.Drawing.Point(104, 78);
+            this.tbYear.Name = "tbYear";
+            this.tbYear.Size = new System.Drawing.Size(100, 20);
+            this.tbYear.TabIndex = 13;
+            // 
+            // tbArtist
+            // 
+            this.tbArtist.Location = new System.Drawing.Point(104, 51);
+            this.tbArtist.Name = "tbArtist";
+            this.tbArtist.Size = new System.Drawing.Size(100, 20);
+            this.tbArtist.TabIndex = 12;
+            // 
+            // tbAlbumName
+            // 
+            this.tbAlbumName.Location = new System.Drawing.Point(104, 23);
+            this.tbAlbumName.Name = "tbAlbumName";
+            this.tbAlbumName.Size = new System.Drawing.Size(100, 20);
+            this.tbAlbumName.TabIndex = 11;
+            // 
+            // lbDescription
+            // 
+            this.lbDescription.AutoSize = true;
+            this.lbDescription.Location = new System.Drawing.Point(16, 140);
+            this.lbDescription.Name = "lbDescription";
+            this.lbDescription.Size = new System.Drawing.Size(60, 13);
+            this.lbDescription.TabIndex = 10;
+            this.lbDescription.Text = "Description";
+            // 
+            // lbImageURL
+            // 
+            this.lbImageURL.AutoSize = true;
+            this.lbImageURL.Location = new System.Drawing.Point(16, 109);
+            this.lbImageURL.Name = "lbImageURL";
+            this.lbImageURL.Size = new System.Drawing.Size(61, 13);
+            this.lbImageURL.TabIndex = 9;
+            this.lbImageURL.Text = "Image URL";
+            // 
+            // lbYear
+            // 
+            this.lbYear.AutoSize = true;
+            this.lbYear.Location = new System.Drawing.Point(16, 81);
+            this.lbYear.Name = "lbYear";
+            this.lbYear.Size = new System.Drawing.Size(29, 13);
+            this.lbYear.TabIndex = 8;
+            this.lbYear.Text = "Year";
+            // 
+            // lbArtist
+            // 
+            this.lbArtist.AutoSize = true;
+            this.lbArtist.Location = new System.Drawing.Point(16, 54);
+            this.lbArtist.Name = "lbArtist";
+            this.lbArtist.Size = new System.Drawing.Size(30, 13);
+            this.lbArtist.TabIndex = 7;
+            this.lbArtist.Text = "Artist";
+            // 
+            // lbAlbumName
+            // 
+            this.lbAlbumName.AutoSize = true;
+            this.lbAlbumName.Location = new System.Drawing.Point(16, 26);
+            this.lbAlbumName.Name = "lbAlbumName";
+            this.lbAlbumName.Size = new System.Drawing.Size(67, 13);
+            this.lbAlbumName.TabIndex = 6;
+            this.lbAlbumName.Text = "Album Name";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(239, 256);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(474, 150);
+            this.dataGridView2.TabIndex = 6;
+            this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellClick);
+            // 
+            // lblTracks
+            // 
+            this.lblTracks.AutoSize = true;
+            this.lblTracks.Location = new System.Drawing.Point(236, 240);
+            this.lblTracks.Name = "lblTracks";
+            this.lblTracks.Size = new System.Drawing.Size(40, 13);
+            this.lblTracks.TabIndex = 7;
+            this.lblTracks.Text = "Tracks";
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(737, 240);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(340, 180);
+            this.webView21.TabIndex = 8;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(239, 412);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(128, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Delete Selected Track";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1089, 438);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.lblTracks);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbAlbumName;
+        private System.Windows.Forms.TextBox tbImageURL;
+        private System.Windows.Forms.TextBox tbYear;
+        private System.Windows.Forms.TextBox tbArtist;
+        private System.Windows.Forms.TextBox tbAlbumName;
+        private System.Windows.Forms.Label lbDescription;
+        private System.Windows.Forms.Label lbImageURL;
+        private System.Windows.Forms.Label lbYear;
+        private System.Windows.Forms.Label lbArtist;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox tbDescription;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label lblTracks;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ DatabaseSQLMusicApp/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaseSQLMusicApp
+{
+    public partial class Form1 : Form
+    {
+
+        BindingSource albumBindingSource = new BindingSource();
+        BindingSource tracksBindingSource = new BindingSource();
+
+        List<Album> albums = new List<Album>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AlbumsDAO albumsDAO = new AlbumsDAO();
+
+            albumBindingSource.DataSource = albumsDAO.getAllAlbums();
+
+            dataGridView1.DataSource = albumBindingSource;
+
+            pictureBox1.Load("https://m.media-amazon.com/images/I/81-SFiRVerL._UF1000,1000_QL80_.jpg");
+
+            // Ensure that the sender is a DataGridView
+            if (sender is DataGridView dataGridView)
+            {
+                int rowClicked = dataGridView.CurrentRow.Index;
+
+                String imageURl = dataGridView1.Rows[rowClicked].Cells[4].Value.ToString();
+
+                pictureBox1.Load(imageURl);
+
+                tracksBindingSource.DataSource = albums[rowClicked].Tracks;
+
+                dataGridView2.DataSource = tracksBindingSource;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AlbumsDAO albumsDAO = new AlbumsDAO();
+
+            albumBindingSource.DataSource = albumsDAO.searchTitles(textBox1.Text);
+
+            dataGridView1.DataSource = albumBindingSource;
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            AlbumsDAO albumsDAO = new AlbumsDAO();
+
+            int rowClicked = e.RowIndex;
+
+            if (rowClicked >= 0 && rowClicked < dataGridView1.Rows.Count)
+            {
+                // Load album image
+                String imageURl = dataGridView1.Rows[rowClicked].Cells[4].Value.ToString();
+                pictureBox1.Load(imageURl);
+
+                // Load tracks for the selected album
+                int albumID = (int)dataGridView1.Rows[rowClicked].Cells[0].Value;
+                tracksBindingSource.DataSource = albumsDAO.getTracksForAlbum(albumID);
+                dataGridView2.DataSource = tracksBindingSource;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Album album = new Album
+            {
+                AlbumName = tbAlbumName.Text,
+                ArtistName = tbArtist.Text,
+                Year = Int32.Parse(tbYear.Text),
+                ImageURL = tbImageURL.Text,
+                Description = tbDescription.Text,
+            };
+
+            AlbumsDAO albums = new AlbumsDAO();
+            int result = AlbumsDAO.addOneAlbum(album);
+            MessageBox.Show(result + "new row(s) inserted");
+        }
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridView dataGridView = (DataGridView)sender;
+
+            int rowClicked = dataGridView.CurrentRow.Index;
+
+            String videoURL = dataGridView.Rows[rowClicked].Cells[3].Value.ToString();
+
+            webView21.Source = new Uri(videoURL);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int rowClicked = dataGridView2.CurrentRow.Index;
+            MessageBox.Show("You clicked row " + rowClicked);
+            int trackID = (int)dataGridView2.Rows[rowClicked].Cells[0].Value;
+            MessageBox.Show("ID of track " + trackID);
+
+            AlbumsDAO albumsDao = new AlbumsDAO();
+
+            int result = albumsDao.deleteTrack(trackID);
+
+            MessageBox.Show("Result " + result);
+
+            dataGridView2.DataSource = null;
+            albums = albumsDao.getAllAlbums();
+        }
+    }
+}
